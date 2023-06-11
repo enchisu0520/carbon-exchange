d14db5a922c74656d7a964e7183e46e2e576aecd
enchisu0520 <enchisu0520@gmail.com>
2023-06-11 20:39:00 +0800

    add search result title

---
+++ src/components/search/SearchResultsTitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="searchResultsTitle" class="bg-gray-100 mb-8">
+        <div class="text-lg flex font-bold mb-2">
+            {{ $t("class.search.result.title") }}
+        </div>
+        <p class="text-base">
+            {{ $t("class.search.result.title.content") }}
+        </p>
+    </div>
+</template>
+
+<script>
+import { defineComponent, toRefs } from 'vue'
+
+export default defineComponent({
+    props: {
+        selectedLang: String
+    },
+    setup(props) {
+        const { selectedLang } = toRefs(props)
+
+        return {
+            selectedLang
+        }
+    }
+})
+</script>
