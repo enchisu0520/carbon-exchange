f38c8b250b4bf441a477afa7ab3118a5b83c7edf
enchisu0520 <enchisu0520@gmail.com>
2023-06-24 09:50:58 +0800

    add mobile cookie consent

--- src/components/shared/CookieConsent.vue
+++ src/components/shared/CookieConsent.vue
@@ -1,24 +1,24 @@
 <template>
 	<div 
-		class="fixed bottom-0 left-0 w-full h-[20vh] bg-gray-200 px-4 py-2"
+		class="fixed bottom-0 left-0 w-full h-[25vh] md:h-[20vh] bg-gray-200 px-4 py-2"
 		:class="{
 			'hidden':
 				!showCookie
 		}"	
 	>
-		<div class="h-full container m-auto flex items-center justify-between">
+		<div class="h-full container mx-auto md:flex md:items-center md:justify-between">
 			<div>
-				<p class="text-xl mb-4">
+				<p class="text-xl my-4 md:mb-4">
 					{{$t("notification")}}
 				</p>
 				<div
-					class="text-base w-[75vw]"
+					class="text-base w-full mb-3 md:w-[75vw]"
 				>
 					{{$t("cookie.msg")}}
 				</div>
 			</div>
 			<button
-				class="text-base bg-amber-500 hover:bg-amber-700 text-white py-2 px-15 rounded w-24"
+				class="text-base bg-blue-900 hover:bg-amber-700 text-white py-2 px-15 rounded-sm w-full md:w-40"
 				@click="acceptCookies"
 			>
 				{{$t("cookie.consent")}}
