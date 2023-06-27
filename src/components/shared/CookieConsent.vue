2f84ca10d912b63ecceb1329b5e8ced763d6b1e2
enchisu0520 <enchisu0520@gmail.com>
2023-06-27 22:54:19 +0800

    adjust cookie consent layout and message

--- src/components/shared/CookieConsent.vue
+++ src/components/shared/CookieConsent.vue
@@ -1,28 +1,32 @@
 <template>
 	<div 
-		class="fixed bottom-0 left-0 w-full h-[25vh] md:h-[20vh] bg-gray-200 px-4 py-2"
+		class="fixed bottom-0 left-0 w-full max-w-fit h-[30vh] md:h-[20vh] bg-gray-200 px-4 py-2"
 		:class="{
 			'hidden':
 				!showCookie
 		}"	
 	>
-		<div class="h-full container mx-auto md:flex md:items-center md:justify-between">
+		<div class="h-full container m-auto md:flex md:flex-col md:w-[85vw]">
 			<div>
 				<p class="text-xl my-4 md:mb-4">
 					{{$t("notification")}}
 				</p>
+			</div>
+			<div
+				class="flex flex-col md:flex md:justify-between"
+			>
 				<div
-					class="text-base w-full mb-3 md:w-[75vw]"
+					class="text-base mb-5 md:w-[70vw]"
 				>
 					{{$t("cookie.msg")}}
 				</div>
+				<button
+					class="text-base bg-blue-900 hover:bg-amber-700 text-white py-2 px-15 rounded-sm w-full md:w-40 md:h-11"
+					@click="acceptCookies"
+				>
+					{{$t("cookie.consent")}}
+				</button>
 			</div>
-			<button
-				class="text-base bg-blue-900 hover:bg-amber-700 text-white py-2 px-15 rounded-sm w-full md:w-40"
-				@click="acceptCookies"
-			>
-				{{$t("cookie.consent")}}
-			</button>
 		</div>
 	</div>
 </template>
