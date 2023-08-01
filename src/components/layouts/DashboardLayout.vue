3d4ef926cb761cd1d2061ee8884d32f4361f58c4
enchisu0520 <enchisu0520@gmail.com>
2023-08-01 23:09:01 +0800

    add dashboard layout

---
+++ src/components/layouts/DashboardLayout.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="min-h-screen text-gray-900 bg-gray-100 dark:bg-dark-bg dark:text-gray-100">
+        <Sidebar 
+            :showDashboardSettingModal="showDashboardSettingModal"
+        />
+        <div
+            style="transition-property: margin; transition-duration: 150ms"
+            :class="[
+                'min-h-screen flex flex-col',
+                {
+                    'lg:ml-64': sidebarState.isOpen,
+                    'md:ml-16': !sidebarState.isOpen,
+                },
+            ]"
+        >
+            <!-- <Navbar /> -->
+
+            <router-view></router-view>
+
+            <DashboardFooter />
+        </div>
+        <DashboardSettingModal
+            :is-open="isDashboardSettingModalOpen"
+            @close-modal="hideDashboardSettingModal"
+        />
+    </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import { sidebarState, toggleDarkMode, isDark } from '../composables'
+import Sidebar from '../sidebar/Sidebar.vue'
+// import Navbar from '@/components/Navbar.vue'
+import DashboardSettingModal from '../dashboard/DashboardSettingModal.vue'
+import DashboardFooter from '../shared/DashboardFooter.vue'
+
+let isDashboardSettingModalOpen = ref(false)
+const showDashboardSettingModal = () => {
+    isDashboardSettingModalOpen.value = true
+}
+
+const hideDashboardSettingModal = () => {
+    isDashboardSettingModalOpen.value = false
+}
+</script>
