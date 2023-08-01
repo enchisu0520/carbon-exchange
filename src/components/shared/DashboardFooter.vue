75db58cc2c64be680c3e2fff84e72df16c19d587
enchisu0520 <enchisu0520@gmail.com>
2023-08-01 23:09:14 +0800

    add dashboard footer

---
+++ src/components/shared/DashboardFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer
+        class="flex flex-col flex-shrink-0 w-full gap-2 px-6 py-4 lg:flex-row lg:justify-between"
+    >
+        <p class="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
+            <span
+                class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+                >© 2023 <a href="#!" class="hover:underline">Carbon Exchange™</a>.
+                All Rights Reserved.
+            </span>
+        </p>
+    </footer>
+</template>
