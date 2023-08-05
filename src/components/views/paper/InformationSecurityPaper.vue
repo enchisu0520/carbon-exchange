dc3001322b4f1e1cfd02cce77da69bb9002c58a6
enchisu0520 <enchisu0520@gmail.com>
2023-08-05 22:55:24 +0800

    remove check box in the information security paper

--- src/components/views/paper/InformationSecurityPaper.vue
+++ src/components/views/paper/InformationSecurityPaper.vue
@@ -203,18 +203,6 @@
 					class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
 				>
 					<tr>
-						<th scope="col" class="p-4">
-							<div class="flex items-center">
-								<input
-									id="checkbox-all-search"
-									type="checkbox"
-									class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-								/>
-								<label for="checkbox-all-search" class="sr-only"
-									>checkbox</label
-								>
-							</div>
-						</th>
 						<th scope="col" class="px-6 py-3 text-sm">級別</th>
 						<th scope="col" class="px-6 py-3 text-sm">類別</th>
 						<th scope="col" class="px-6 py-3 text-sm">項目</th>
@@ -236,20 +224,6 @@
 						:key="index"
         				v-for="(question, index) in tableData"
 					>
-						<td class="w-4 p-4">
-							<div class="flex items-center">
-								<input
-									id="checkbox-table-search-1"
-									type="checkbox"
-									class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-								/>
-								<label
-									for="checkbox-table-search-1"
-									class="sr-only"
-									>checkbox</label
-								>
-							</div>
-						</td>
 						<td
 							scope="row"
 							class="px-6 py-4 font-medium text-gray-900 dark:text-white"
