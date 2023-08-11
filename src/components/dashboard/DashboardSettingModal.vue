7627f4af8fb9162724b37dd7a90a95fad4e8220f
enchisu0520 <enchisu0520@gmail.com>
2023-08-11 23:09:52 +0800

    fix dark status

--- src/components/dashboard/DashboardSettingModal.vue
+++ src/components/dashboard/DashboardSettingModal.vue
@@ -45,7 +45,7 @@
 					<label
 						class="relative inline-flex items-center mb-5 cursor-pointer"
 					>
-						<input type="checkbox" value="" class="sr-only peer" />
+						<input type="checkbox" v-model="darkStatus" class="sr-only peer" />
 						<div
 							class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
 							@click="toggleDarkMode()"
@@ -57,7 +57,7 @@
 	</div>
 </template>
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import {
 	isDark,
 	toggleDarkMode,
@@ -68,8 +68,15 @@ export default defineComponent({
 		isOpen: Boolean,
 	},
 	setup() {
+		let darkStatus = ref(false)
+
+		onMounted(() => {
+            darkStatus.value = isDark.value
+        })
+		
 		return {
 			isDark,
+			darkStatus,
 			toggleDarkMode,
 		};
 	},
