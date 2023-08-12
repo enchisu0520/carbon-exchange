54246b5de44044ff968e6002d0d3b9d5d0c3ea3e
enchisu0520 <enchisu0520@gmail.com>
2023-08-12 10:49:48 +0800

    adjust dashboard footer

--- src/components/shared/DashboardFooter.vue
+++ src/components/shared/DashboardFooter.vue
@@ -1,13 +1,22 @@
 <template>
-    <footer
-        class="flex flex-col flex-shrink-0 w-full gap-2 px-6 py-4 lg:flex-row lg:justify-between"
-    >
-        <p class="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
-            <span
-                class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-                >© 2023 <a href="#!" class="hover:underline">Carbon Exchange™</a>.
-                All Rights Reserved.
-            </span>
-        </p>
-    </footer>
+	<footer
+		class="flex flex-col flex-shrink-0 w-full gap-2 px-6 py-4 lg:flex-row lg:justify-between"
+	>
+		<p
+			class="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400"
+		>
+			<span
+				class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+				>© 2023
+				<router-link
+					to="/dashboard"
+					class="inline-flex items-center gap-2"
+				>
+					<span class="hover:underline"
+						>Simple Auditing™</span
+					> </router-link
+				>. All Rights Reserved.
+			</span>
+		</p>
+	</footer>
 </template>
